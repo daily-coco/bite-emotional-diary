d7d2df77b6196aa5d4aaec34674309b296edded6
daily-coco <dailydev.sw@gmail.com>
2025-08-21 15:30:35 +0900

    feat:New 페이지 기능 구현

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,6 +1,9 @@
+import { useNavigate } from 'react-router-dom';
 import Button from './Button';
 import './Editor.css';
 import EmotionItem from './EmotionItem';
+import { useState } from 'react';
+
 // 이것도 외부로 빼서 관리할 수 있는 부분이니깐 빼두기 (고도화때)
 const emotionList = [
   {
@@ -25,13 +28,79 @@ const emotionList = [
   },
 ];
 
-const Editor = () => {
-  const emotionId = 1;
+// [4] 작성하기 버튼 관련 이벤트 연결을 위한 프롭스 추가 (*onSubmit)
+const Editor = ({ onSubmit }) => {
+  // const emotionId = 1; // 임시용(확인용)
+  //[1] 사용자가 input에 입력한 값들을 저장하는 state
+
+  // state에 여러개의 값을 저장할 때는 useState를 객체로 만들어서 데이터를 저장한다.
+  const [input, setInput] = useState({
+    createDate: new Date(),
+    emotionId: 3,
+    content: '',
+  });
+
+  // 취소하기 버튼 클릭 시
+  const nav = useNavigate();
+
+  //[2-1] 문자열로 데이터 객체 변환
+  const getStringDate = (targetDate) => {
+    //날짜 - YYYY-MM-DD
+    let year = targetDate.getFullYear();
+    let month = targetDate.getMonth() + 1;
+    let date = targetDate.getDate();
+
+    // 날짜 자리수 맞춰주기(10 이하)
+    if (month < 10) {
+      month = `0${month}`;
+    }
+    if (date < 10) {
+      date = `0${date}`;
+    }
+    return `${year}-${month}-${date}`;
+  };
+  //[2-2] 변경될 때마다 데이터 픽커 값 반영 및 저장될 수 있도록 이벤트 핸들러 추가
+  const onChangeInput = (e) => {
+    console.log(e.target.name); // 어떤 요소의 입력이 들어왔는지 체크
+    console.log(e.target.value); // 현재 입력된 값이 무엇인지?
+
+    let name = e.target.name;
+    let value = e.target.value;
+
+    if (name === 'createDate') {
+      // input State 안의 객체는 값들이 객체타입이고, 특히 날짜 경우에는 Date 형태이다보니 지금 픽커에서 입력한 값은 '문자'이기 떄문에 이를 그대로 넣어주면 안되기 때문에 형변환을 내부에서 업데이트 전에 처리해준다.
+      value = new Date(value);
+    }
+    setInput({
+      ...input, // 기존 state 객체 input 유지
+      [name]: value, // 업데이트된 객체의 키:벨류/ 값 업데이트
+    });
+  };
+
+  //[4] onSubmit
+  const onClickSubmitButton = () => {
+    onSubmit(input);
+  };
+
+  // [임시용] 고도화 때 레이어팝업으로 변경하기! 로직 까먹을 수도 있을 것을 대비해 confirm으로 임시 UI/Ux처리
+  const onClickCancel = () => {
+    const confirmData = confirm('정말로 작성하던 일기를 그만 쓰겠습니까?');
+    if (confirmData) {
+      nav(-1);
+    }
+  };
+
   return (
     <div className='Editor'>
       <section className='date_section'>
         <h4>오늘의 날짜</h4>
-        <input type='date' />
+        <input
+          type='date'
+          name='createDate'
+          onChange={onChangeInput}
+          value={getStringDate(input.createDate)}
+        />
+        {/* [2] input은 date로된 객체로된 값을 이해하지 못하기 때문에 getStringDate를 통해서 변환된 값을 넣어준다.  */}
       </section>
 
       <section className='emotion_section'>
@@ -46,9 +115,17 @@ const Editor = () => {
           {emotionList.map((item) => {
             return (
               <EmotionItem
+                onClick={() =>
+                  onChangeInput({
+                    target: {
+                      name: 'emotionId',
+                      value: item.emotionId,
+                    },
+                  })
+                }
                 key={item.emotionId}
                 {...item}
-                isSelected={item.emotionId === emotionId}
+                isSelected={item.emotionId === input.emotionId}
               />
             );
           })}
@@ -57,12 +134,22 @@ const Editor = () => {
 
       <section className='content_section'>
         <h4>오늘의 일기</h4>
-        <textarea placeholder='오늘은 어땠나요?'></textarea>
+        <textarea
+          placeholder='오늘은 어땠나요?'
+          name='content'
+          //[3] 이벤트 연결
+          onChange={onChangeInput}
+        ></textarea>
       </section>
 
       <section className='button_section'>
-        <Button text={'저장하기'} />
-        <Button text={'취소하기'} />
+        <Button text={'취소하기'} onClick={onClickCancel} />
+        {/* Editor 컴포넌트 경우에는 수정과 신규 일기 생성에 같이 이용되고 있다. 이점을 유의해서 이벤트핸들러를 만들어줘야한다. */}
+        <Button
+          onClick={onClickSubmitButton}
+          text={'작성하기'}
+          type={'positive'}
+        />
       </section>
     </div>
   );
--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,14 +1,29 @@
 import Header from '../components/Header';
 import Button from '../components/Button';
 import Editor from '../components/Editor';
+import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import { DiaryDispatchContext } from '../App';
+
 const New = () => {
+  const nav = useNavigate();
+  const { onCreate } = useContext(DiaryDispatchContext);
+  const onSubmit = (input) => {
+    //input은 Editor에 State로 정의한 input State
+    onCreate(input.createDate.getTime(), input.emotionId, input.content);
+    nav('/', { replace: true }); // 작성 완료 후 홈으로 이동
+    // useNavigate(to, option) : option 자리에 replace를 적어주면
+    // 1) to로 적은 url로 이동 + 2) 뒤로 가기 방지 시키는 옵션임
+  };
+
   return (
     <div>
       <Header
         title={'새 일기 쓰기'}
-        leftChild={<Button text={'< 뒤 로가기'} />}
+        leftChild={<Button text={'< 뒤로가기'} onClick={() => nav(-1)} />}
       />
-      <Editor />
+
+      <Editor onSubmit={onSubmit} />
     </div>
   );
 };
