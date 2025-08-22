5476b59ff7d0cbc15357709c7896c01dfb2e62ce
daily-coco <dailydev.sw@gmail.com>
2025-08-22 10:47:24 +0900

    feat:localstorage 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,76 @@
 import './App.css';
-import { useReducer, useRef, createContext } from 'react';
+import { useReducer, useRef, createContext, useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import New from './pages/New';
 import Diary from './pages/Diary';
 import Edit from './pages/Edit';
 import Notfound from './pages/Notfound';
+
 function reducer(state, action) {
+  let nextState; // localstorage에 변경된 데이터를 보관해주기용
+
   switch (action.type) {
-    case 'CREATE':
-      return [action.data, ...state];
-    case 'UPDATE':
-      return state.map((item) =>
+    case 'INIT': {
+      return action.data; // localstorage의 값을 바로 불러온 거기 때문에 nextState에 저장하지 않는 것.
+    }
+    case 'CREATE': {
+      nextState = [action.data, ...state];
+      break;
+    }
+    case 'UPDATE': {
+      nextState = state.map((item) =>
         String(item.id) === String(action.data.id) ? action.data : item
       );
-    case 'DELETE':
-      return state.filter((item) => String(item.id) !== String(action.id));
+      break;
+    }
+    case 'DELETE': {
+      nextState = state.filter((item) => String(item.id) !== String(action.id));
+      break;
+    }
     default:
       return state;
   }
+  localStorage.setItem('diary', JSON.stringify(nextState));
+  return nextState;
 }
 
 export const DiaryStateContext = createContext();
 export const DiaryDispatchContext = createContext();
 
 function App() {
-  const idRef = useRef(3);
-  const [data, dispatch] = useReducer(reducer, mockData);
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, dispatch] = useReducer(reducer, []);
+  const idRef = useRef(0);
+
+  // 컴포넌트가 첫 마운트 시 diary 저장된 데이터를 가져와서 셋팅해주기
+  useEffect(() => {
+    const storedData = localStorage.getItem('diary');
+    if (!storedData) {
+      setIsLoading(false);
+      return;
+    }
+    const parsedData = JSON.parse(storedData);
+
+    let maxId = 0;
+    if (!Array.isArray(parsedData)) {
+      setIsLoading(false);
+      return;
+    }
+    parsedData.forEach((item) => {
+      //localstroage의 값이 기본적으로 문자형이기 때문에 형변환을 통한 정확한 계산이 되도록
+      if (Number(item.id) > maxId) {
+        maxId = Number(item.id);
+      }
+    });
+    idRef.current = maxId + 1;
+
+    dispatch({
+      type: 'INIT',
+      data: parsedData,
+    });
+    setIsLoading(false);
+  }, []);
 
   const onCreate = (createDate, emotionId, content) => {
     dispatch({
@@ -58,6 +102,10 @@ function App() {
   };
 
   //Storage
+
+  if (isLoading) {
+    return <div>데이터 로딩중 ...</div>;
+  }
 
   return (
     <>
